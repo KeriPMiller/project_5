433d95380b2a843ea5f4f04801187e5f2b4f0afd
Kerianne Miller <keriannemiller@kerianne-millers-computer.local>
2015-04-09 13:53:05 -0400

    made external javascript cheat sheet with 3 working functions

---
+++ pj5.js
@@ -0,0 +1,109 @@
+/*
+// variables
+	var x = 120;
+	var y = 9;
+	var z = x+y;
+
+// debugging
+	console.log();
+
+//Different data types
+ numbers
+
+ 	var num = 34;
+
+ strings
+
+ 	var name = "Keri"
+
+ booleans (true and false statements)
+	 (10>9)
+	 (5 === 5)
+	 (5 <= 2)
+
+//Arrays
+	var colors = ["red","blue","yellow"];
+	var flowers = ["carnation", "rose", "dasiy"];
+
+//Testing
+	console.log();
+	alert();
+
+//Logic
+	=== equal
+	!== not equal
+	> greater than
+	>= greater or equal
+	< less than
+	<= less or equal
+
+//Functions
+	function nameOf (a, b){
+		return a * b;
+		}
+	nameOf(4,3);
+*/
+/*
+// ex 1
+ var x = 3;
+ var y = 9;
+ var z = x+y;
+
+alert(z);
+
+// ex 2
+	if (10 === 10) {
+		alert("Yay!");
+	} else {
+		alert("something is broken!");
+	}
+
+
+// ex 3
+	var colors = ["red","orange","yellow","green",
+					"blue","indigo","violet"];
+
+	for(i = 0; i < colors.length; i++){
+		console.log(colors[i]);
+	}
+*/
+
+// function 1
+function myFunction1 (a,b){
+	alert( a * b);
+}
+
+myFunction1(4,5);
+
+// function 2
+
+var x = "Good Morning";
+var y = "how are you?";
+
+function myFunction2 (x,y){
+	console.log(x + " " + y);
+}
+
+myFunction2(x,y);
+
+// function 3
+
+var name = prompt ("What is your name?")
+
+function myFunction3(name){
+	console.log(name + "?" + " " +"What a cool name!");
+}
+
+myFunction3(name);
+
+
+
+
+
+
+
+
+
+
+
+
